64b22f7715675d986bcf94bc337295b6d23ed8d3
Erkan Yıldırım <erkanyildirim28@gmail.com>
2021-11-01 21:06:00 +0300

    first

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kondlama.io");
+
+
+//js type safe degildir
+let dolarDun=9.20;
+ dolarDun="9.20"
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun);
+
+ const euroDun=11.2
+
+//euroDun=11 const birkere deger eklenir sınrasinda bir daha deger atilamaz
+
+//degiskenler camelCasing olarak yazili ilk hark kucuk oalrak baslar
+
+//PaskalCasing
+
+
+//array
+let konutKredileri=[12,"Emlak Konut Kredisi",["a","b","c"]]
+
+let konutKredileri2=["Konut kredisi","Emlak Konut Kredisi","Kamu Komut Kredisi"]
+
